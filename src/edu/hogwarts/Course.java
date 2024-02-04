729b3d123ef4d687f27a11c893bb5c049eac4a58
osman-butt <butt.osman@gmail.com>
2024-02-04 21:12:02 +0100

    Added Course class

---
+++ src/edu/hogwarts/Course.java
@@ -0,0 +1,41 @@
+package edu.hogwarts;
+
+public class Course {
+    private Subject subject;
+    private Teacher teacher;
+    private Student[] students;
+
+    public Course(Subject subject, Teacher teacher, Student[] students) {
+        this.subject = subject;
+        this.teacher = teacher;
+        this.students = students;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public void setSubject(Subject subject) {
+        this.subject = subject;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public Student[] getStudents() {
+        return students;
+    }
+
+    public void setStudents(Student[] students) {
+        this.students = students;
+    }
+
+    public String toString() {
+        
+    }
+}
