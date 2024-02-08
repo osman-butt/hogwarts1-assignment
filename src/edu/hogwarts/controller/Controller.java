53d8af411ebfdc5de4d9dab4631d155ba6246058
osman-butt <butt.osman@gmail.com>
2024-02-08 15:45:33 +0100

    added controller interface

---
+++ src/edu/hogwarts/controller/Controller.java
@@ -0,0 +1,11 @@
+package edu.hogwarts.controller;
+import java.util.ArrayList;
+import java.util.UUID;
+
+public interface Controller<T> {
+    void create(T object);
+    T get(UUID id);
+    ArrayList<T> getAll();
+    void update(UUID id,T object);
+    void delete(UUID id,T object);
+}
