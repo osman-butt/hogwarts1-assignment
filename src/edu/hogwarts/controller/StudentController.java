65b8b6e57bcf70a2c8ca657e8cb4d8eb614e527e
osman-butt <butt.osman@gmail.com>
2024-02-08 21:01:58 +0100

    Added CRUD for StudentController

--- src/edu/hogwarts/controller/StudentController.java
+++ src/edu/hogwarts/controller/StudentController.java
@@ -3,34 +3,46 @@ package edu.hogwarts.controller;
 import edu.hogwarts.model.HogwartsStudent;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.UUID;
 
 public class StudentController implements Controller<HogwartsStudent>{
 
-    public StudentController() {}
+    private HashMap<UUID,HogwartsStudent> students;
+
+    public StudentController() {
+        // Initialize the list of students
+        students = new HashMap<>();
+    }
 
     @Override
-    public void create(HogwartsStudent object) {
-
+    public void create(HogwartsStudent student) {
+        students.put(student.getId(),student);
     }
 
     @Override
     public HogwartsStudent get(UUID id) {
-        return null;
+        return students.get(id);
     }
 
     @Override
     public ArrayList<HogwartsStudent> getAll() {
-        return null;
+        return new ArrayList<>(students.values());
     }
 
     @Override
     public void update(UUID id, HogwartsStudent object) {
-
+        HogwartsStudent updatedStudent = students.get(id);
+        updatedStudent.setFullName(object.getFullName());
+        updatedStudent.setHouse(object.getHouse());
+        updatedStudent.setTeams(object.getTeams());
+        updatedStudent.setEnrollmentYear(object.getEnrollmentYear());
+        updatedStudent.setGraduationYear(object.getGraduationYear());
+//        updatedStudent.setPrefect(); ADD PREFECT
     }
 
     @Override
-    public void delete(UUID id, HogwartsStudent object) {
-
+    public void delete(UUID id) {
+        students.remove(id);
     }
 }
