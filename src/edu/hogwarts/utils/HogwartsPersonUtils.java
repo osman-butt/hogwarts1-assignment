a1b32bee93c8191bf92252178a7f6e126e8dca43
osman-butt <butt.osman@gmail.com>
2024-02-11 23:43:31 +0100

    Added filter method

--- src/edu/hogwarts/utils/HogwartsPersonUtils.java
+++ src/edu/hogwarts/utils/HogwartsPersonUtils.java
@@ -1,15 +1,18 @@
 package edu.hogwarts.utils;
 
+import edu.generic.RoleType;
 import edu.hogwarts.model.HogwartsPerson;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class HogwartsPersonUtils {
 
-//    Method to sort an ArrayList of HogwartsPerson.
-//    The method mutates the original ArrayList
-    public static void sort(ArrayList<HogwartsPerson> personList, SortNameType sortName, boolean isDecending) {
+//    Method to sort a List of HogwartsPerson.
+//    The method mutates the original List
+    public static void sort(List<HogwartsPerson> personList, SortNameType sortName, boolean isDecending) {
         Comparator<HogwartsPerson> comparator;
         switch (sortName) {
             case SortNameType.FIRST_NAME:
@@ -39,5 +42,29 @@ public class HogwartsPersonUtils {
         }
         // Sorting the list
         personList.sort(comparator);
+    }
+
+//    Method to filter a List of HogwartsPerson on HouseType and RoleType.
+//    The method does not mutate the original List
+    public static List<HogwartsPerson> filter(List<HogwartsPerson> personList, HouseType house, RoleType role) {
+        if(house != null && role == null) {
+            return filter(personList,house);
+        }
+        if (house == null && role != null) {
+            return filter(personList,role);
+        }
+        return personList.stream().filter(person -> person.getHouse().toString().equals(house.toString()) && person.getRole().toString().equals(role.toString())).collect(Collectors.toList());
+    }
+
+//    Method to filter a List of HogwartsPerson on HouseType.
+//    The method does not mutate the original List
+    public static List<HogwartsPerson> filter(List<HogwartsPerson> personList, HouseType house) {
+        return personList.stream().filter(person -> person.getHouse().toString().equals(house.toString())).collect(Collectors.toList());
+    }
+
+//    Method to filter a List of HogwartsPerson on RoleType.
+//    The method does not mutate the original List
+    public static List<HogwartsPerson> filter(List<HogwartsPerson> personList, RoleType role) {
+        return personList.stream().filter(person -> person.getRole().toString().equals(role.toString())).collect(Collectors.toList());
     }
 }
