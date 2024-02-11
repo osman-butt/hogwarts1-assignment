abe7a40771aaf10331164fb0259e9741ccf58d59
osman-butt <butt.osman@gmail.com>
2024-02-11 23:44:44 +0100

    Refractoring

--- src/edu/hogwarts/application/Application.java
+++ src/edu/hogwarts/application/Application.java
@@ -2,13 +2,15 @@ package edu.hogwarts.application;
 
 import edu.hogwarts.controller.StudentController;
 import edu.hogwarts.controller.TeacherController;
+import edu.hogwarts.view.UserInterface;
 
 public class Application {
 
     // Controller attributes
-    TeacherController teacherController = new TeacherController();
-    StudentController studentController = new StudentController();
+    TeacherController teacherController;
+    StudentController studentController;
     InitApp initApp;
+    UserInterface userInterface;
 
     public static void main(String[] args) {
         Application app = new Application();
@@ -16,9 +18,13 @@ public class Application {
     }
 
     public void start() {
+        studentController = new StudentController();
+        teacherController = new TeacherController();
         // InitApp should use dependency injection
         initApp = new InitApp(studentController,teacherController);
         initApp.initData();
+        userInterface = new UserInterface(studentController,teacherController);
+        userInterface.start();
     }
 
 }
